261d22fce523283dccd092e304cce4accc322e7f
Fabio Carlesso <fabio.carlesso@gmail.com>
2024-01-27 17:07:44 -0300

    feat(recarga): Create first module Recarga

---
+++ recarga/src/test/java/com/fabiocarlesso/recargaexpresstyrant/recarga/RecargaApplicationTests.java
@@ -0,0 +1,9 @@
+package com.fabiocarlesso.recargaexpresstyrant.recarga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecargaApplicationTests {
+
+}
